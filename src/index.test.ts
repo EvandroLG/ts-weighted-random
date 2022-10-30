07bc9ed123047c187df2d94845192dc376f9ca2b
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2022-10-30 11:35:39 +0100

    adds basic unit tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,26 @@
 import weightedRandom from ".";
 
-test("", () => {
-  expect(true).toBe(true);
+test("picks value at random", () => {
+  const map = new Map([
+    [1, 1],
+    [2, 1],
+    [3, 1],
+  ]);
+
+  const result = weightedRandom(map);
+  expect(map.has(result)).toBeTruthy();
+});
+
+test("picks sole item when map is unity length", () => {
+  const map = new Map([[1, 1]]);
+
+  const result = weightedRandom(map);
+  expect(result).toBe(1);
+});
+
+test("returns undefined when map is empty", () => {
+  const map = new Map([]);
+
+  const result = weightedRandom(map);
+  expect(result).toBeUndefined();
 });
